a3c078660e2a38f624335dea3ca7bf3deb14533b
corynko <ethankotel@gmail.com>
2024-10-10 21:45:49 -0600

    built out initial light/dark mode functionality and theming

---
+++ src/components/miscComponents/switchModeButton.tsx
@@ -0,0 +1,33 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import DarkIcon from "@mui/icons-material/Brightness4";
+import LightIcon from "@mui/icons-material/Brightness7";
+import React from "react";
+
+import { ColorContext } from "../../contexts/ColorContext";
+
+export const SwitchModeButton = () => {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorContext);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        minHeight: "100vh",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        transition: "background-color 0.1s, color 0.1s",
+      }}
+    >
+      {theme.palette.mode} mode
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? <LightIcon /> : <DarkIcon />}
+      </IconButton>
+    </Box>
+  );
+};
---
+++ src/themes/themeOptions.ts
@@ -0,0 +1,16 @@
+
+
+// export const appTheme = createTheme({
+//   palette: {
+//     primary: {
+//       // light: blue[300],
+//       main: "#000000",
+//       // dark: blue[700],
+//     },
+//     secondary: {
+//       // light: pink[300],
+//       main: "#11cb5f",
+//       // dark: pink[700],
+//     },
+//   },
+// });

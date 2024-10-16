01c9ffa5882d6a4324aee446b9d0c2db1df4d079
corynko <ethankotel@gmail.com>
2024-10-16 00:50:28 -0600

    initial styling of home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,26 @@
+import pilotPic from "../../assets/images/png/pilotCustomFineOnPaper.png";
 import { Box } from "@mui/material";
 
 const HomePage = () => {
   return (
     <>
-      <Box sx={{ zIndex: 5, position: "relative" }}>Hello!</Box>
+      <Box
+        className="flex row between relative"
+        sx={{
+          zIndex: 5,
+          width: "100vw",
+          // height: "100vh",
+          alignItems: "center",
+        }}
+      >
+        <div className="flex">
+          <img src={pilotPic} className="homeHeaderPic" />
+        </div>
+        <div className="flex">
+          <h1>welcome</h1>
+        </div>
+      </Box>
+      <Box>hello</Box>
     </>
   );
 };

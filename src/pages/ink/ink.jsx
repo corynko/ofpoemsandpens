bd7c3cb690f1a7e8fc4d257557d1d0e5d5dd0718
corynko <ethankotel@gmail.com>
2024-10-25 16:58:30 -0600

    clean gh-pages branch

--- src/pages/ink/ink.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export default function Ink() {
-  return (
-    <>
-      <Box>Hello!</Box>
-    </>
-  );
-}
--- src/pages/paper/paper.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export default function Paper() {
-  return (
-    <>
-      <Box>Hello!</Box>
-    </>
-  );
-}

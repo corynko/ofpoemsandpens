532c48c31f2276dc84511a93a9472317796738ed
corynko <ethankotel@gmail.com>
2024-10-28 14:24:58 -0600

    added logic to dynamically alter link previews of sent poem links

--- src/pages/poems/poems.jsx
+++ src/pages/poems/poems.jsx
@@ -1,5 +1,7 @@
 import { Box } from "@mui/material";
 import PoemCard from "../../components/poemsComponents/poemCards";
+import { Helmet } from "react-helmet";
+import { useParams } from "react-router-dom";
 
 //poem image imports
 
@@ -87,18 +89,56 @@ const poemsArray = [
 ];
 
 export default function Poems() {
+  const { poemId } = useParams();
+  const poem = poemsArray.find((p) => p.urlAppend === poemId);
+
   return (
-    <Box className="flex wrap center">
-      {poemsArray.map((poem, index) => (
-        <PoemCard
-          key={index}
-          title={poem.title}
-          image={poem.image}
-          transcription={poem.transcription}
-          author={poem.author}
-          urlAppend={poem.urlAppend}
+    <>
+      <Helmet>
+        <title>
+          {poem
+            ? `Read ${poem.title}, a poem on Of Pens and Poems`
+            : "Of Pens and Poems"}
+        </title>
+        <meta
+          property="og:title"
+          content={
+            poem
+              ? `Read ${poem.title}, a poem on Of Pens and Poems`
+              : "Of Pens and Poems"
+          }
         />
-      ))}
-    </Box>
+        <meta
+          property="og:description"
+          content={
+            poem
+              ? `${poem.transcription.slice(0, 100)}...` // Take the first 100 characters for a brief description
+              : "Explore unique poems and writings."
+          }
+        />
+        <meta
+          property="og:image"
+          content={poem ? poem.image : "/favicon.ico"}
+        />
+        <meta
+          property="og:url"
+          content={`https://ofpensandpoems.com/poems/${poemId || ""}`}
+        />
+        <meta property="og:type" content="article" />
+      </Helmet>
+
+      <Box className="flex wrap center">
+        {poemsArray.map((poem, index) => (
+          <PoemCard
+            key={index}
+            title={poem.title}
+            image={poem.image}
+            transcription={poem.transcription}
+            author={poem.author}
+            urlAppend={poem.urlAppend}
+          />
+        ))}
+      </Box>
+    </>
   );
 }

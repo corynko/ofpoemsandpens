cf7d166df46d556002a2d51ff22c01e97205464b
corynko <ethankotel@gmail.com>
2025-01-05 17:51:30 -0700

    added functionality for converting assets into webp's on build to reduce total package size, decrease load times, many changes to package.json to update packages and reduce vulnerabilities, fix those introduced by installing imagemin plugins

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
+import viteImagemin from "@vheemstra/vite-plugin-imagemin";
+import imageminGifsicle from "imagemin-gifsicle";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminOptipng from "imagemin-optipng";
+import imageminWebp from "imagemin-webp";
 
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [
+    react(),
+    commonjs(),
+    viteImagemin({
+      verbose: true,
+      onlyAssets: true,
+      plugins: {
+        jpg: imageminMozjpeg({ quality: 95 }),
+        png: imageminOptipng({
+          optimizationLevel: 7,
+          bitDepthReduction: false,
+          colorTypeReduction: true,
+          paletteReduction: false,
+        }),
+      },
+      makeWebp: {
+        skipIfLargerThan: "optimized",
+        plugins: {
+          jpg: imageminWebp({ preset: "photo", quality: 95, method: 6 }),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   base: "/",
   define: {
     global: {},
